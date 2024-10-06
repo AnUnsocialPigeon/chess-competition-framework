60e140f3071b7a4fb54d26376b2c0fd7a2042fdc
AnUnsocialPigeon <jdturrell@gmail.com>
2024-10-06 15:46:29 +0100

    Made comments better

--- agressor.py
+++ agressor.py
@@ -1,70 +1,65 @@
 import chess
 import random
 
-# Piece values for determining which piece to capture, based on standard chess piece values.
-# The higher the value, the more desirable to capture
 PIECE_VALUES = {
     chess.PAWN: 1,
     chess.KNIGHT: 3,
     chess.BISHOP: 3,
     chess.ROOK: 5,
     chess.QUEEN: 9,
-    chess.KING: 100  # You can't really capture the king, but it's there for reference.
+    chess.KING: 100
 }
 
-# Function to prioritize moves based on checkmate, checks, and captures
 def prioritize_moves(board):
     legal_moves = list(board.legal_moves)
     
-    # First, check if we can deliver checkmate
+    # Can we checkmate?
     for move in legal_moves:
         board.push(move)
         if board.is_checkmate():
             board.pop()
-            return move  # Immediate checkmate is the highest priority
+            return move
         board.pop()
     
-    # Second, look for checks by capturing a piece
+    # Can we check by capture?
     capture_checks = []
     for move in legal_moves:
-        if board.is_capture(move):  # Check if it's a capture
+        if board.is_capture(move):  
             board.push(move)
-            if board.is_check():  # Check if it also delivers check
+            if board.is_check():  
                 capture_checks.append(move)
             board.pop()
     if capture_checks:
-        # If there are any capture checks, return one at random (could improve by choosing highest-value capture)
         return random.choice(capture_checks)
+    
+    # Can we check?
+    for move in legal_moves:
+        board.push(move)
+        if board.is_check():
+            board.pop()
+            return move  
+        board.pop()
 
-    # Third, prioritize moves that capture the highest value piece
+    # Can we capture? (highest value = priority)
     captures = []
     for move in legal_moves:
         if board.is_capture(move):
             captured_piece = board.piece_at(move.to_square)
             if captured_piece is not None:
                 captures.append((move, PIECE_VALUES.get(captured_piece.piece_type, 0)))
-    
     if captures:
-        # Sort captures by piece value and return the highest one
         captures.sort(key=lambda x: x[1], reverse=True)
-        return captures[0][0]  # Return the move that captures the most valuable piece
-
-    # Fourth, prioritize checks (without capturing)
-    for move in legal_moves:
-        board.push(move)
-        if board.is_check():
-            board.pop()
-            return move  # Deliver check if possible
-        board.pop()
-
-    # Finally, if none of the above, just return a random move
+        return captures[0][0]  
+    
+    # Else, random move
     return random.choice(legal_moves)
 
-# Get FEN string
+
+# Take in input
 fen_input = input()
 board = chess.Board(fen_input)
 
-print(f"Improved bot got: {fen_input}")
+print(f"Agressive bot got: {fen_input}")
 
 # Get the best move according to our priorities
 best_move = prioritize_moves(board)
@@ -74,3 +69,4 @@ print(f"I'm gonna do {best_move}")
 # Apply the chosen move
 board.push(best_move)
 print(board.fen())  # The last print statement of the bot must be purely the FEN string of the resulting board.
+
